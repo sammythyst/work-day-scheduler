de135587097e145913ed9a2fb583bbc9b54bff67
sasw <saswain96@gmail.com>
2022-10-17 15:11:50 -0500

    color coded time slots and added time recognition

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # work-day-scheduler
 Challenge 5 Third Party APIs
+
+https://sammythyst.github.io/work-day-scheduler/
+![Screenshot of finished project page](./assets/scheduler%20finished.png)
+
+## Description
+The purpose of this project is to allow users to plan and schedule their day and to visibly see where they're at in their itinerary. Tasks are entered by the user in one of the hour-long slots available to them. 
+
+## Usage
+As users want to plan their day, they will be able to input different tasks into an allotted text field associated with the time they want to complete the task. To save the task, click the save button next to the text field and your task will be saved locally to your device. The task will remain there as long as it is saved to the device. The gray timeslots represent time in the day that has already passed, the red indicated the current hour, and the green represents the hours in the day yet to come.
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,52 @@
-// display current day and date with moment()
-var currentDay = moment().format("dddd, MMMM Do")
-$("#currentDay").text(currentDay);
+$(document).ready(function () {
 
-// color coding time-blocks based on past, present, or future
+    // display current day and date with moment()
+    var currentDay = moment().format("dddd, MMMM Do, h:mm a")
+    $("#currentDay").text(currentDay);
 
 
-// using localStorage to create and save tasks throughout the day
-$("#9a .description").val(localStorage.getItem("9a"));
-$("#10a .description").val(localStorage.getItem("10a"));
-$("#11a .description").val(localStorage.getItem("11a"));
-$("#12p .description").val(localStorage.getItem("12p"));
-$("#1p .description").val(localStorage.getItem("1p"));
-$("#2p .description").val(localStorage.getItem("2p"));
-$("#3p .description").val(localStorage.getItem("3p"));
-$("#4p .description").val(localStorage.getItem("4p"));
-$("#5p .description").val(localStorage.getItem("5p"));
+    // using localStorage to create and save tasks throughout the day
+    $("#hour9 .description").val(localStorage.getItem("hour9"));
+    $("#hour10 .description").val(localStorage.getItem("hour10"));
+    $("#hour11 .description").val(localStorage.getItem("hour11"));
+    $("#hour12 .description").val(localStorage.getItem("hour12"));
+    $("#hour13 .description").val(localStorage.getItem("hour13"));
+    $("#hour14 .description").val(localStorage.getItem("hour14"));
+    $("#hour15 .description").val(localStorage.getItem("hour15"));
+    $("#hour16 .description").val(localStorage.getItem("hour16"));
+    $("#hour17 .description").val(localStorage.getItem("hour17"));
 
-// save button
-$(".saveBtn").on("click", function () {
-    var text = $(this).siblings(".description").val();
-    var time = $(this).parent().attr("id");
+    // save button
+    $(".saveBtn").on("click", function () {
+        var text = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
 
-    localStorage.setItem(time, text);
+        localStorage.setItem(time, text);
+    })
+
+    // color coding time-blocks based on past, present, or future
+    function currentHour() {
+        var momentHour = moment().hour();
+
+        $(".time-block").each(function() {
+            var blockHour = parseInt($(this).attr("id").split("hour")[1]);
+            console.log(blockHour, momentHour)
+
+            if (blockHour < momentHour){
+                $(this).addClass("past");
+                $(this).removeClass("present");
+                $(this).removeClass("future");
+            } else if (blockHour === momentHour) {
+                $(this).removeClass("past");
+                $(this).addClass("present");
+                $(this).removeClass("future");
+            } else {
+                $(this).removeClass("past");
+                $(this).removeClass("present");
+                $(this).addClass("future");
+            }
+        })
+    }
+        currentHour();
+
 })
