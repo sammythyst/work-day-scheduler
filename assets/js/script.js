891f61deb07d09582df0f3860222abd7de8215c9
sasw <saswain96@gmail.com>
2022-11-08 16:24:26 -0600

    condensed js code, made easier to read

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,48 +5,46 @@ $(document).ready(function () {
     $("#currentDay").text(currentDay);
 
 
-    // using localStorage to create and save tasks throughout the day
-    $("#hour9 .description").val(localStorage.getItem("hour9"));
-    $("#hour10 .description").val(localStorage.getItem("hour10"));
-    $("#hour11 .description").val(localStorage.getItem("hour11"));
-    $("#hour12 .description").val(localStorage.getItem("hour12"));
-    $("#hour13 .description").val(localStorage.getItem("hour13"));
-    $("#hour14 .description").val(localStorage.getItem("hour14"));
-    $("#hour15 .description").val(localStorage.getItem("hour15"));
-    $("#hour16 .description").val(localStorage.getItem("hour16"));
-    $("#hour17 .description").val(localStorage.getItem("hour17"));
+    // local storage saving custom description of day's plans
+    function planDay() {
+        $(".hour").each(function() {
+            var blockHour = $(this).text();
+            var schedule = localStorage.getItem(blockHour);
 
-    // save button
-    $(".saveBtn").on("click", function () {
-        var text = $(this).siblings(".description").val();
-        var time = $(this).parent().attr("id");
+            if(schedule !== null) {
+                $(this).siblings(".description").val(schedule);
+            }
+        })
+    }
 
-        localStorage.setItem(time, text);
-    })
 
     // color coding time-blocks based on past, present, or future
     function currentHour() {
         var momentHour = moment().hour();
 
         $(".time-block").each(function() {
-            var blockHour = parseInt($(this).attr("id").split("hour")[1]);
+            var blockHour = parseInt($(this).attr("id"));
             console.log(blockHour, momentHour)
 
             if (blockHour < momentHour){
                 $(this).addClass("past");
-                $(this).removeClass("present");
-                $(this).removeClass("future");
             } else if (blockHour === momentHour) {
-                $(this).removeClass("past");
                 $(this).addClass("present");
-                $(this).removeClass("future");
             } else {
-                $(this).removeClass("past");
-                $(this).removeClass("present");
                 $(this).addClass("future");
             }
         })
     }
-        currentHour();
 
+    
+    // save button
+    $(".saveBtn").on("click", function () {
+        var text = $(this).siblings(".description").val();
+        var time = $(this).siblings(".hour").text();
+
+        localStorage.setItem(time, text);
+    })
+
+    currentHour();
+    planDay();
 })
